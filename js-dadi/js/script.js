e738155fd2d74e81f34a4050fb3c64168f0dab4a
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-14 18:37:39 +0200

    fnalized styles and logic

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -4,17 +4,36 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
 
 
 
-//prendo il bottone dall'html
+
 const button = document.getElementById('button');
+const result = document.getElementById('output-result')
 //aggiungo l'evento 'click' al bottone
 button.addEventListener('click', function(){
 
-    //estraggo il numero casuale
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
-    console.log(randomNumber)
-    //prendo gli output dei numeri dall'html
+    //estraggo il numero casuale per l'utente
+    const randomNumberUser = Math.floor(Math.random() * 6) + 1;
+    console.log(randomNumberUser);
 
+    //estraggo il numero casuale per il computer
+    const randomNumberComputer = Math.floor(Math.random() * 6) +1;
+    console.log('-------', randomNumberComputer);
 
-})
+    //prendo gli output dall'html
+    let userNumber = document.querySelector('.output-nr-user');
+    let computerNumber = document.querySelector('.output-nr-computer'); 
+
+    //stampo il risultato in pagina
+    computerNumber.innerHTML = randomNumberComputer;
+    userNumber.innerHTML = randomNumberUser;
+   
+    //creo la condizione di vittoria
+    if(randomNumberUser > randomNumberComputer){
+        result.innerHTML = 'Hai vinto umano.'
+    }else if(randomNumberUser < randomNumberComputer){
+        result.innerHTML = '(*voce robotica*) Ha-ha-ha il...computer...vince..'
+    }   
+
+});
+
 
     
